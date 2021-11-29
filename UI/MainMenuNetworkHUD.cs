06a502b215d4626b70978c7c87cc785103a26c9d
MzKcf4 <kachunfu4@gmail.com>
2021-11-29 21:48:33 +0800

    Add start (offline) scene

---
+++ UI/MainMenuNetworkHUD.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenuNetworkHUD : MonoBehaviour
+{
+    public Button btnHost;
+    public Button btnConnect;
+    public TextMeshProUGUI inputFieldAddress;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (NetworkManager.singleton.networkAddress != "localhost") { inputFieldAddress.text = NetworkManager.singleton.networkAddress; }
+        
+        btnHost.onClick.AddListener(HostServer);
+        btnConnect.onClick.AddListener(ConnectToServer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void HostServer()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+    
+    public void ConnectToServer()
+    {
+        NetworkManager.singleton.networkAddress = inputFieldAddress.text;
+        NetworkManager.singleton.StartClient();
+    }
+}
