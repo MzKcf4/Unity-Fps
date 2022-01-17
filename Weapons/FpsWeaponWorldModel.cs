39533760f22f22e114a3351d288b8058645185da
MzKcf4 <kachunfu4@gmail.com>
2022-01-18 01:01:21 +0800

    Fix empty weapon muzzleTransform

--- Weapons/FpsWeaponWorldModel.cs
+++ Weapons/FpsWeaponWorldModel.cs
@@ -22,9 +22,12 @@ public class FpsWeaponWorldModel : MonoBehaviour
     void Start()
     {
         GameObject muzzleFeedbackPrefab = WeaponAssetManager.Instance.weaponMuzzleFeedbackPrefab;
-        GameObject muzzleFeedbackObj = Instantiate(muzzleFeedbackPrefab,muzzleTransform);
-        muzzleFeedbackObj.transform.localPosition = Vector3.zero;
-        muzzleFeedbacks = muzzleFeedbackObj.GetComponent<MMFeedbacks>();
+        if (muzzleTransform != null)
+        {
+            GameObject muzzleFeedbackObj = Instantiate(muzzleFeedbackPrefab, muzzleTransform);
+            muzzleFeedbackObj.transform.localPosition = Vector3.zero;
+            muzzleFeedbacks = muzzleFeedbackObj.GetComponent<MMFeedbacks>();
+        }
     }
 
     // Update is called once per frame
@@ -40,7 +43,7 @@ public class FpsWeaponWorldModel : MonoBehaviour
     
     public void ShootProjectile(Vector3 dest)
     {
-        if(muzzleFeedbacks)
+        if(muzzleFeedbacks && muzzleTransform)
             muzzleFeedbacks.PlayFeedbacks();
     }
 }
