620893535569130f26411eb96880916968d9e3a2
MzKcf4 <kachunfu4@gmail.com>
2022-08-07 15:52:43 +0800

    Add commit MonsterModeConfig

---
+++ DTO/Config/MonsterModeConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MonsterModeConfig
+{
+    public int stageTargetKillCount = 40;
+    public int restTime = 10;
+    public int maxMonsters = 7;
+    public int maxStage = 4;
+    public float healthMultiplierPerStage = 2.0f;
+    public float healthMultiplierPerPlayer = 1.2f;
+}
+
